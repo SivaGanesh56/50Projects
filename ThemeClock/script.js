4daf9d1222699e8a593111a399191b496a07950f
SivaGanesh56 <siva.ganesh584@gmail.com>
2021-12-14 09:38:20 +0530

    theme clock added

---
+++ ThemeClock/script.js
@@ -0,0 +1,59 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+});
+
+
+const setTime = () => {
+    const time = new Date();
+    let month = time.getMonth();
+    let day = time.getDay();
+    const hours = time.getHours();
+    const hoursForClock = hours%12;
+    let minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const date = time.getDate();
+
+    const ampm = hours >= 12 ? 'PM': 'AM';
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`;
+
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    month = months[month];
+    day = days[day];
+
+   
+
+    timeEl.innerHTML = `${hours}:${minutes} ${ampm}`;
+    dateEl.innerHTML = `${day}, ${month} <span class='circle'>${date}</span>`
+}
+
+
+
+
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+setInterval(setTime, 1000);
